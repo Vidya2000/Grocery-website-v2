fe32e4e6a22306488c96fa874fd9c0c89292fc62
vidyagms9634 <22803649-vidyagms9634@users.noreply.replit.com>
2023-05-05 06:22:14 +0000

    Modified

--- app.py
+++ app.py
@@ -2,10 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+ITEMS = [
+  {
+    'id':1,
+    'name':'ToorDal',
+    'price':'120',
+    'Weight':'1kg'
+  },
+  {
+    'id':1,
+    'name':'Almond',
+    'price':'330',
+    'Weight':'500g'
+  },
+{
+    'id':1,
+    'name':'Peanuts',
+    'price':'100',
+    'Weight':'1kg'
+  },
+  {
+    'id':1,
+    'name':'MoongDal',
+    'price':'80',
+    'Weight':'1kg'
+  },
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', items=ITEMS)
 
 
 if __name__ == "__main__":
